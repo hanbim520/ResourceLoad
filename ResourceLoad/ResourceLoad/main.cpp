08f0fdcc3d66c37d453d6517bca9c79b990c29d4
710605420 <710605420@qq.com>
2019-08-25 19:28:43 +0800

    添加pb和sqlite3

--- ResourceLoad/ResourceLoad/main.cpp
+++ ResourceLoad/ResourceLoad/main.cpp
@@ -11,9 +11,22 @@
 #include <iostream>
 #include <fstream>
 #include <mutex>
+#include <cstddef>
 
 #include "../Code/Loader.hpp"
+#include "DataInfoProto.pb.h"
 
+bool hasWrite = false;
+
+void WriteFile(JxSDK::DataInfo datas)
+{
+	std::string szFileName = "../ClientLog.txt";
+	std::fstream output(szFileName, std::ios::out | std::ios::binary | std::ios::app);
+	datas.SerializePartialToOstream(&output);
+	output.flush();
+	output.close();
+	hasWrite = true;
+}
 EasyLoader::Loader *_loader = new  EasyLoader::Loader();
 void DoTest()
 {
@@ -26,8 +39,10 @@ void DoTest()
 	for (int i = 0; i < 1000; ++i)
 	{
 #if TARGET_WIN
-		void* file = _loader->LoadMetadataFile("I:/Github/ResourceLoad/1.mkv");
+		void* file = _loader->LoadMetadataFile("I:/Github/ResourceLoad/ClientLog.txt");
 		//	char* content = (char *)file;
+		JxSDK::DataInfo dataTmp;
+		dataTmp.ParseFromArray(file, sizeof(JxSDK::DataInfo));
 #else
 		void* file = _loader->LoadMetadataFile("/Users/zhanghaijun/Desktop/TENCENT_MSDKUnity_ALL_3.2.27.1_99388.zip");
 		char* content = (char *)file;
@@ -39,7 +54,7 @@ void DoTest()
 	finish = clock();
 	totaltime = (double)(finish - start) / CLOCKS_PER_SEC;
 	std::cout << "mmap cost time: " << totaltime << " ms" << std::endl;
-	return;
+	
 	// std::cout << content;
 	char data[1000000];
 	std::ifstream infile;
@@ -62,10 +77,25 @@ void DoTest()
 	finish = clock();
 	totaltime = (double)(finish - start) / CLOCKS_PER_SEC;
 	std::cout << "normal cost time: " << totaltime << " ms"<< std::endl;
-	// 在屏幕上写入数据
-   // std::cout << data << std::endl;
-
+	
+	if (hasWrite)return;
+	JxSDK::DataInfo dataPb;
+	dataPb.set_mapstr("fdfdsafaddfaas");
+	dataPb.set_width(1024);
+	dataPb.set_height(512);
+	dataPb.set_size(1022);
+	std::cout << sizeof(JxSDK::DataInfo) << std::endl;
+	char * pData = new char[sizeof(JxSDK::DataInfo)];
+	dataPb.SerializePartialToArray(pData, sizeof(JxSDK::DataInfo));
+	if (sizeof(pData) == 0)
+		std::cout << "error" << std::endl;
+	//byte * pData2 = new byte[sizeof DataInfo];
+	JxSDK::DataInfo dataTmp;
+	dataTmp.ParseFromArray(pData, sizeof(JxSDK::DataInfo));
+	WriteFile(dataTmp);
 }
+
+
 int main(int argc, const char * argv[]) {
 	char ch = getchar();
 	while (std::cin >> ch) {  //输入没有结束，cin 就返回 true，条件就为真
